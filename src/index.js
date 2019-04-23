5cf3c9095dcec41e78090ab125dd8805896ed6bf
Marat <marat@deex.exchange>
2019-04-24 00:58:45 +0300

    added modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import getGreeting from "./greet";
 import "./styles.css";
 console.log(getGreeting("world"));
+
+const btn = document.getElementById("open");
+const close = document.getElementById("close");
+const modal = document.getElementById("modal");
+
+btn.addEventListener("click", event => {
+  event.preventDefault();
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", event => {
+  event.preventDefault();
+  modal.style.display = "none";
+});
