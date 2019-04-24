cdfcbf54fd1c938d31218bb38335046a327ec3c3
Zhurmukhamedov Marat <zhurmukhamedov.m@readyforsky.com>
2019-04-24 18:13:40 +0300

    added Prettier FormaterDocument

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import getGreeting from "./greet";
-import "./styles.css";
-console.log(getGreeting("world"));
+import getGreeting from './greet'
+import './styles.css'
+console.log(getGreeting('world'))
 
-const btn = document.getElementById("open");
-const close = document.getElementById("close");
-const modal = document.getElementById("modal");
+const btn = document.getElementById('open')
+const close = document.getElementById('close')
+const modal = document.getElementById('modal')
 
-btn.addEventListener("click", event => {
-  event.preventDefault();
-  modal.style.display = "block";
-});
+btn.addEventListener('click', event => {
+  event.preventDefault()
+  modal.style.display = 'block'
+})
 
-close.addEventListener("click", event => {
-  event.preventDefault();
-  modal.style.display = "none";
-});
+close.addEventListener('click', event => {
+  event.preventDefault()
+  modal.style.display = 'none'
+})
