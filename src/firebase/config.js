22c1a39fbb0b413575c632f7510cf2e82588cdd1
Zhurmukhamedov Marat <zhurmukhamedov.m@readyforsky.com>
2019-06-24 16:01:57 +0300

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCCkDxE13JAhA97JX6lWQKhFKAW6gNXUbQ',
+  authDomain: 'otau-market.firebaseapp.com',
+  databaseURL: 'https://otau-market.firebaseio.com',
+  projectId: 'otau-market',
+  storageBucket: '',
+  messagingSenderId: '374457147704',
+  appId: '1:374457147704:web:f65553820650ebbd'
+}
+
+export default firebaseConfig
